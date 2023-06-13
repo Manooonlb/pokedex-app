689de6f3e7ef39e56095e396d74a52002c6532fd
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-06-13 14:56:55 +0200

    add pokemonList const

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,27 @@
-function PokemonCard(){
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbi"></img>
-        <figcaption>bulbasaur</figcaption>
-    </figure>
-}
-
+function PokemonCard() {
+    
+    const pokemon = pokemonList[1];
+    return (
+      <figure>
+        {pokemon.imgsrc ? (
+          <img src={pokemon.imgsrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+    
+}   
+const pokemonList = [
+    {
+        name : 'bulbasaur',
+        imgsrc : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name : 'mew',
+    },
+];
 export default PokemonCard;
+
+
